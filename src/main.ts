45f6ffe65742b8a2d48250facf4a3a31c0775717
SharpDevOps10 <td090833@gmail.com>
2023-07-11 21:59:43 +0300

    Add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-async function start () {
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const start = async () => {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Topoviy Backend')
+    .setDescription('Documentation')
+    .setVersion('1.0.0')
+    .addTag('Rero')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  await app.listen(PORT, () => console.log(`Server has started on port = ${PORT}`));
 
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
-}
+};
+
 start();
