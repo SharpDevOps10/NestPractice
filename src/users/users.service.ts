07faff2ac88075f5b3c92a6390fabbfa2e4b894f
SharpDevOps10 <td090833@gmail.com>
2023-07-15 22:15:08 +0300

    Bugfix: fixe `empty roles` in response

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,6 +22,12 @@ export class UsersService {
   }
 
   async getAllUsers () {
-    return await this.userRepository.findAll();
+    return await this.userRepository.findAll(
+      {
+        include: {
+          all: true,
+        },
+      },
+    );
   }
 }
