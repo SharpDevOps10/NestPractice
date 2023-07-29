7f3c9cc7137719e77e1e9e46eca7a0295b79fc7f
SharpDevOps10 <td090833@gmail.com>
2023-07-29 23:50:10 +0300

    Use ValidationPipe globally

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from './pipes/ValidationPipe';
+
 const start = async () => {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,9 @@ const start = async () => {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(PORT, () => console.log(`Server has started on port = ${PORT}`));
 
 };
