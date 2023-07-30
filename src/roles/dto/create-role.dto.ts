896d83d09b342ee42358e715734d121a22a4e59f
SharpDevOps10 <td090833@gmail.com>
2023-07-30 23:22:30 +0300

    Add create role validation(dto)

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from 'class-validator';
+
 export class CreateRoleDto {
+  @IsString({ message: 'Role must be a string' })
   readonly value: string;
+  @IsString({ message: 'Description must be a string' })
   readonly description: string;
 }
