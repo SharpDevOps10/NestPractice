1b778b0583a615cd929256ff7053caf1d2487535
SharpDevOps10 <td090833@gmail.com>
2023-07-31 23:41:42 +0300

    Add `FilesService`

---
+++ src/files/FilesService.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as uuid from 'uuid';
+import { WritingFileException } from '../exceptions/WritingFileException';
+@Injectable()
+export class FilesService {
+
+  async createFile (file): Promise<string> {
+    try {
+      const fileName = uuid.v4() + '.jpg';
+      const filePath = path.resolve(__dirname, '..', 'static');
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+      fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+      return fileName;
+    } catch (e) {
+      throw new WritingFileException();
+    }
+  }
+
+
+
+}
