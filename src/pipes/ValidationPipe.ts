c3313251bcd613f8f7f02e5f1907d92538dbdf56
SharpDevOps10 <td090833@gmail.com>
2023-08-03 23:32:06 +0300

    Refactor: - change deprecated plainToClass to plainToInstance

--- src/pipes/ValidationPipe.ts
+++ src/pipes/ValidationPipe.ts
@@ -1,5 +1,5 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { ValidationException } from '../exceptions/ValidationException';
 
@@ -7,7 +7,7 @@ import { ValidationException } from '../exceptions/ValidationException';
 @Injectable()
 export class ValidationPipe implements PipeTransform {
   async transform (value: any, metadata: ArgumentMetadata): Promise<any> {
-    const obj = plainToClass(metadata.metatype, value);
+    const obj = plainToInstance(metadata.metatype, value);
     const errors = await validate(obj);
 
     if (errors.length) {
